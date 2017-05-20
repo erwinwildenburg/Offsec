3c9bd8de5daee00e26f5af7fe9cb44147280a037
Erwin Wildenburg <mail@erwinwildenburg.net>
2017-05-20 16:09:58 -0400

    Add portknocking script

---
+++ Scripts/port-knocking.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ "$#" -ne 4 ]; then
+	echo "Usage: $0 <ip> <port1> <port2> <port3>"
+	exit 1
+fi
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+
+nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
