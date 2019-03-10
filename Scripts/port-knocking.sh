f400d5f39706894ed1c1ab13df9064390d445b4f
Erwin Wildenburg <mail@erwinwildenburg.net>
2019-03-10 15:29:15 +0100

    Add sleeps in the portknocking script
    If you don't sleep then the port knocking doesn't work as it sees it as part of the sequence.

--- Scripts/port-knocking.sh
+++ Scripts/port-knocking.sh
@@ -7,22 +7,27 @@ fi
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+sleep 5
 
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
+sleep 5
 
 nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
+sleep 5
 
 nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+sleep 5
 
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $3 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
+sleep 5
 
 nmap -Pn --host_timeout 500 --max-retries 0 -p $4 $1
 nmap -Pn --host_timeout 500 --max-retries 0 -p $2 $1
